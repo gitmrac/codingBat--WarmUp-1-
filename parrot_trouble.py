dea4548e76fb09ef705511ea3fe54bad11e185b6
Parag Garg <36436348+gitmrac@users.noreply.github.com>
2018-11-08 18:09:43 +0530

    Create parrot_trouble.py

---
+++ parrot_trouble.py
@@ -0,0 +1,15 @@
+#We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
+#We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+
+def parrot_trouble(talking, hour):
+  if talking==True and (hour>=0 and hour <24):
+    if hour<7 or hour>20:
+      return True
+    else:return False
+  else:
+    return False
+
+
+parrot_trouble(True, 6) 
+parrot_trouble(True, 7) 
+parrot_trouble(False, 6) 
